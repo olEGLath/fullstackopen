cef5635bfe62ae59622ae616c5642c234997e526
Esdras Daniel <esdrasdaniel1016@gmail.com>
2025-10-14 15:11:00 -0300

    part 1a exercises 1.1-1.2

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,47 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part name={props.part1} exercises={props.exercises1} />
+      <Part name={props.part2} exercises={props.exercises2} />
+      <Part name={props.part3} exercises={props.exercises3} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Desenvolvimento de aplicação Half Stack'
+  const part1 = 'Fundamentos da biblioteca React'
+  const exercises1 = 10
+  const part2 = 'Usando props para passar dados'
+  const exercises2 = 7
+  const part3 = 'Estado de um componente'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+    </div>
+  )
+}
+
+export default App
